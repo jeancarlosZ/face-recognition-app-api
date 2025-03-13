9921af4823cc19afb8d49bb9832abb3f73dcbc6a
jeancarlosZ <64503277+jeancarlosZ@users.noreply.github.com>
2025-03-13 18:39:43 -0400

    added database support for endpoints and tested with Postman

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const cors = require("cors");
+const knex = require("knex");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: 5432,
+    user: "jeancarloszhongchen",
+    password: "",
+    database: "face-recognition-api"
+  },
+});
 
 const image = require("./controllers/image");
 
@@ -10,84 +22,86 @@ const saltRounds = 10;
 app.use(express.json());
 app.use(cors());
 
-let idCount = 1;
-const mockDatabase = {
-  users: []
-}
+app.get("/", (req, res) => {
+  res.send(db.users);
+})
 
-// Test only - when you have a database variable you want to use
-// app.get("/", (req, res) => {
-//   res.send(mockDatabase.users);
-// })
-
-app.post("/signin", async (req, res) => {
+app.post("/signin", (req, res) => {
   const { email, password } = req.body;
-  for (let i = 0; i < mockDatabase.users.length; i++) {
-    const user = mockDatabase.users[i];
-    if (email === user.email) {
-      const match = await bcrypt.compare(password, user.password);
-      if (match) {
-        return res.json(user);
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", email)
+    .then(async (data) => {
+      const isValid = await bcrypt.compare(password, data[0].hash);
+      if (isValid) {
+        return db.select("*")
+          .from("users")
+          .where("email", "=", email)
+          .then(user => res.json(user[0]))
+          .catch(err => res.status(400).json("Unable to get user."));
+      } else {
+        return res.status(400).json("Wrong credentials.");
       };
-    };
-  };
-  res.status(400).json("wrong credentials");
+    })
+    .catch(err => res.status(400).json("Wrong credentials."));
 })
 
 app.post("/register", (req, res) => {
   const { name, email, password } = req.body;
-  mockDatabase.users.push({
-    id: String(idCount++),
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date()
-  })
-  const user = mockDatabase.users[mockDatabase.users.length - 1];
-  bcrypt.hash(password, saltRounds, function (err, hash) {
-    if (err) {
-      console.log(err);
-    } else {
-      user.password = hash;
-      console.log(user);
-    };
-  });
-  res.json(user);
+  bcrypt.hash(password, saltRounds)
+    .then(function (hash) {
+      db.transaction(trx => {
+        trx.insert({
+          hash: hash,
+          email: email
+        })
+          .into("login")
+          .returning("email")
+          .then(loginEmail => {
+            return trx("users")
+              .returning("*")
+              .insert({
+                name: name,
+                email: loginEmail[0].email,
+                joined: new Date()
+              })
+              .then(user => res.json(user[0]));
+          })
+          .then(trx.commit)
+          .catch(trx.rollback);
+      })
+        .catch(err => res.status(400).json("If you already have an account, please sign in instead."));
+    })
+    .catch(err => res.status(400).json("The email and password fields need to be valid and not empty."));
 })
 
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
-  let found = false;
-  mockDatabase.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    };
-  })
-  if (!found) {
-    res.status(400).json("no such user");
-  };
+  db.select("*")
+    .from("users")
+    .where("id", "=", id)
+    .then(user => {
+      if (user.length) {
+        return res.json(user[0]);
+      } else {
+        return res.status(400).json("Not found.");
+      };
+    })
+    .catch(err => res.status(400).json("Error getting user."));
 })
 
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  let found = false;
-  mockDatabase.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    };
-  })
-  if (!found) {
-    res.status(400).json("no such user");
-  };
+  db.select("*")
+    .from("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => res.json(entries[0].entries))
+    .catch(err => res.status(400).json("Unable to get entries."));
 })
 
-app.post("/imageurl", (req, res) => {
-  image.handleApiCall(req, res);
-})
+app.post("/imageurl", (req, res) => { image.handleApiCall(req, res) })
 
 app.listen(3000, () => {
   console.log("app is running on port 3000");
